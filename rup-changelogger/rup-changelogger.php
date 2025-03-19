6be482d6a0326553564a3f4699233518e14d4c72
stingray82 <11146912+stingray82@users.noreply.github.com>
2025-03-19 17:26:39 +0000

    Added Updates Pushed version to 1.01

--- rup-changelogger/rup-changelogger.php
+++ rup-changelogger/rup-changelogger.php
@@ -5,7 +5,7 @@
  * Description:       A simple shortcode generation for remote text files in to changelogs
  * Requires at least: 6.5
  * Requires PHP:      8.0
- * Version:           1.0
+ * Version:           1.01
  * Author:            reallyusefulplugins.com
  * Author URI:        https://reallyusefulplugins.com
  * License:           GPL2
@@ -23,6 +23,28 @@ if (!defined('ABSPATH')) {
 define('RUP_CHANGELOGGER_RUP_CHANGELOGGER_VERSION', '1.0');
 define('RUP_CHANGELOGGER_RUP_CHANGELOGGER_DIR', plugin_dir_path(__FILE__));
 define('RUP_CHANGELOGGER_RUP_CHANGELOGGER_URL', plugin_dir_url(__FILE__));
+
+$plugin_prefix = 'RUPCHANGELOGG';
+
+// Extract the version number
+$plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
+
+// Plugin Constants
+define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
+define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
+define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
+define($plugin_prefix . '_VER', $plugin_data['Version']);
+define($plugin_prefix . '_CACHE_KEY', 'rupchangelogg-cache-key-for-plugin');
+define($plugin_prefix . '_REMOTE_URL', 'https://reallyusefulplugins.com/wp-content/plugins/hoster/inc/secure-download.php?file=json&download=840&token=92c6ec56bab2e9096f27ad82007ab16accb92c6e06682709eed49306dad3a262');
+
+require constant($plugin_prefix . '_PATH') . 'includes/update.php';
+
+new RUPCHANGELOGG_DPUpdateChecker(
+    constant($plugin_prefix . '_BASE'),
+    constant($plugin_prefix . '_VER'),
+    constant($plugin_prefix . '_CACHE_KEY'),
+    constant($plugin_prefix . '_REMOTE_URL'),
+);
 
 // Include functions
 require_once RUP_CHANGELOGGER_RUP_CHANGELOGGER_DIR . 'includes/functions.php';

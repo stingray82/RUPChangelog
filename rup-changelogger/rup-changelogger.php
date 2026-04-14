183df5fa3a00147433f6a86afb22120aac236084
stingray82 <11146912+stingray82@users.noreply.github.com>
2026-04-14 21:32:31 +0100

    Version 1.30 Release

--- rup-changelogger/rup-changelogger.php
+++ rup-changelogger/rup-changelogger.php
@@ -5,7 +5,7 @@
  * Tested up to:      6.9.4
  * Requires at least: 6.5
  * Requires PHP:      8.0
- * Version:           1.30-beta
+ * Version:           1.30
  * Author:            reallyusefulplugins.com
  * Author URI:        https://reallyusefulplugins.com
  * License:           GPL2
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
     exit; // Prevent direct access
 }
 
-define('RUP_Changelogger_VERSION', '1.30-beta');
+define('RUP_Changelogger_VERSION', '1.30');
 
 // Include functions
  require_once __DIR__ . '/includes/functions.php';
